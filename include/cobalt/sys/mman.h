5c4ea832ef204528264adbe67482dbc48b76d6a5
Philippe Gerum <rpm@xenomai.org>
2011-09-19 16:41:20 +0200

    cobalt: introduce wrapped/non-wrapped POSIX call annotation
    __RT() and __STD() macros are introduced to enclose POSIX calls which
    should go through the corresponding Cobalt real-time wrapper
    (__wrap_*) or directly to the standard glibc routine (__real_*)
    respectively.
    
    At this chance, we sanitize the declarations of all routines we
    overload in the Cobalt interface, providing prototypes for both the
    wrapped and non-wrapped forms, all grouped in the new
    include/cobalt/wrappers.h file.
    
    The intent is to implement self-documenting and non-ambiguous code
    using explicit annotation of POSIX calls, with respect to the actual
    call we want depending on the underlying real-time core
    (cobalt/mercury).
    
    We make the Cobalt API use such annotation as appropriate, other
    interfaces will follow.

--- include/cobalt/sys/mman.h
+++ include/cobalt/sys/mman.h
@@ -54,35 +54,36 @@ int munmap(void *addr, size_t len);
 #else /* !(__KERNEL__ || __XENO_SIM__) */
 
 #include_next <sys/mman.h>
+#include <cobalt/wrappers.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int __real_shm_open(const char *name, int oflag, mode_t mode);
+COBALT_DECL(int, shm_open(const char *name, int oflag, mode_t mode));
 
-int __real_shm_unlink(const char *name);
+COBALT_DECL(int, shm_unlink(const char *name));
 
 #if !defined(_FILE_OFFSET_BITS) || _FILE_OFFSET_BITS != 64
-void *__real_mmap(void *addr,
-		  size_t len,
-		  int prot,
-		  int flags,
-		  int fildes,
-		  long off);
+COBALT_DECL(void *, mmap(void *addr,
+			 size_t len,
+			 int prot,
+			 int flags,
+			 int fildes,
+			 long off));
 #else
 #define __real_mmap __real_mmap64
 #endif
 #ifdef _LARGEFILE64_SOURCE
-void *__real_mmap64(void *addr,
-		    size_t len,
-		    int prot,
-		    int flags,
-		    int fildes,
-		    long long off);
+COBALT_DECL(void *, mmap64(void *addr,
+			   size_t len,
+			   int prot,
+			   int flags,
+			   int fildes,
+			   long long off));
 #endif
 
-int __real_munmap(void *addr, size_t len);
+COBALT_DECL(int, munmap(void *addr, size_t len));
 
 #ifdef __cplusplus
 }
